de2d86d8320b2ef8badde5b328aaa21f2adf3e0d
jsancheg <jalfre81@hotmail.com>
2022-02-05 00:35:21 +0000

    Create utilities.R
    utilities

---
+++ utilities.R
@@ -0,0 +1,61 @@
+library(readxl)
+library(skimr)
+library(rgl)
+library(stringi)
+library(stringr)
+library(fda)
+library(chemometrics)
+library(scatterplot3d)
+library(RandPro)
+library(class)
+library(VGAM)
+library(nnet)
+library(scales)
+library(pls) # partial least square
+#library(mixOmics) # partial least square - discriminant analysis
+library(ROCR)
+library(plyr)
+library(tictoc)
+library(pROC)
+library(caret)
+library(e1071) # SVM
+library(class) # k nearest neighborhood
+library(gmodels) # CrossTable
+library(fda)
+library(ggplot2)
+library(gridExtra) # combine plots
+library(ggthemes)
+library(ggfortify)
+library(tidyverse)
+library(pls)
+library(fastDummies)
+library(factoextra) # PCA , Factor analysis
+library(mdatools) # SIMCA
+library(nnet) # Multinomial regression
+library(glmnet) # Multinomial regression with regularization
+library(DIRECT)# Multinormal distribution
+#work_path <- "E:/University of Glasgow/Second Year/Literature review/R Code/"
+library(nlme) # Correlation structure
+library(MixSim) # Simulating multinomial distribution
+library(MASS)   # Linear Mixed Models
+library(lme4)   # Linear Mixed Models
+library(gee)    # Linear Mixed Models Generalised estimating equations
+library(forecast) # time series function autoarima
+library(tseries) # time series
+library(dplyr)
+library(MBESS) # cor2cov
+library(chemometrics)
+library(ContaminatedMixt)# Mixture of contaminated normal distributions
+library(mnormt)
+library(clustvarsel) # Mixture of normal distributions
+library(parallel)
+library(parallelly)
+library(doParallel)
+library(BMA) # function bicreg
+library(mclust)
+rm(list = ls())
+
+WorkPath <- getwd()
+source("functions.r")
+source("load_datasets.r")
+
