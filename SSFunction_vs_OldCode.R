8928176b5a8376d817ad11547bb17363f5a3af97
jsancheg <jalfre81@hotmail.com>
2024-01-22 09:29:34 +0000

    CreateSSFiles2Groups
    Create SemiSupervised Files for scenarios with 2 groups and 100 variables

---
+++ SSFunction_vs_OldCode.R
@@ -0,0 +1,11 @@
+
+
+#SSV_2_2_5_3000_75_BAL_SCBSV_VD_A8080_E2020_10
+
+
+# pathSSFiles
+pathSFiles <- "E:/University of Glasgow/Thesis/SFiles/"
+file_name <-"SSV_2_2_5_3000_75_BAL_SCBSV_VD_A8080_E2020_10"
+Sfile <- readRDS(paste0(pathSFiles,file_name,".RDS"))
+
+Sfile
