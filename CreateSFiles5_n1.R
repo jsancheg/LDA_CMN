9e1afb003aa0a556767eff931628b54ca5c31208
jsancheg <jalfre81@hotmail.com>
2024-02-06 22:57:51 +0000

    correctionSemisupervisedfunctions

--- CreateSFiles5_n1.R
+++ CreateSFiles5_n1.R
@@ -1,11 +1,14 @@
 # 
-
+sys_info <- Sys.info()
+if(sys_info["nodename"] == "WildFree")
+  setwd("/home/jsancheg/git_environment/LDA_CMN/")
 
 source("Semisupervised.R")
-source("ListScenariosFiles1.R")
+source("ListScenarios.R")
 source("GSFile.R")
 library(purrr)
 library(ContaminatedMixt)
+library(ssh)
 
 if (!requireNamespace("googledrive", quietly = TRUE)) {
   install.packages("googledrive")
@@ -20,7 +23,7 @@ library(readr)
 # Windows path
 system_info <- Sys.info()
 #OS_name <- system_info("")
-pc_name <- system_info['modename'] 
+pc_name <- system_info['nodename'] 
 
 if(pc_name == "LAPTOP-ADR3M911")
 {
@@ -36,29 +39,44 @@ if(pc_name == "LAPTOP-ADR3M911")
   
 }
 
-
+#ScenariosTBF <- readRDS("Scenarios100varTBFit.RDS")
+#Scenarios100S <- ScenariosTBF$SFiles_Scenarios
+#Scenarios100SS <-ScenariosTBF$SSFiles_Scenarios
+  
 dir(pathScenarios)
-ini <- 1
-fin <- n5.1
+ini <- n100.8
+fin <- n100.9
 #fin <- n2.5
-fin
+fin-ini +1
 
 tic("File 5 variables")
+my_ssh_session <- ssh_connect("2201449s@130.209.66.82")
 
-LastFilename <- NULL
-CountFilesProcessed <- 0
-
-
-mclapply(Scenarios2.5[ini:fin], function(x){
-  LastFilename <- x  
-  SFilename <- str_replace(x,"S_","SV_")
-  FilesProcessed <- dir(pathSSFiles)
-  if(is_empty(intersect(FilesProcessed,SFilename))) 
-  {
-    GenerateSFile(x,pathScenarios,pathSFiles) 
-    CountFilesProcessed <- CountFilesProcessed + 1
-  } else cat("\n The file ",SSFilename, " already exists in the directory. \n")
+  mclapply(Scenarios100[ini:(ini+3)], function(x)
+    {
+    if(pc_name == "WildFree")
+    {
+      if(is_empty(intersect(x,dir(pathScenarios))) )
+          scp_download(my_ssh_session, paste0("/home/pgrad1/2201449s/R/CMN/Scenarios3/",x),                             pathScenarios )
+    }
+    
+    tryCatch(
+      {
+        SFilename <- str_replace(x,"S_","SV_")
+        FilesProcessed <- dir(pathSFiles)
+        if(is_empty(intersect(FilesProcessed,SFilename))) 
+        {
+          GenerateSFile(x,pathScenarios,pathSFiles) 
+        } else cat("\n The file ",SFilename, " already exists in the directory. \n")
+        return(1)
+      }, error = function(e)
+      {
+        cat("Error processing scenario: ",x , " \n")
+        return(NULL)
+      }
+    )
+    
+  }, mc.cores = 1) 
   
-}, mc.cores = 1) 
 
 toc()
