deab1ee0868fc6d396dad885a45168b661f31c2e
jsancheg <jalfre81@hotmail.com>
2024-02-12 11:27:38 +0000

    Code to Generate SFiles

---
+++ CreateSFiles100_n4.R
@@ -0,0 +1,57 @@
+# 
+source("Semisupervised.R")
+source("ListScenarios.R")
+source("GSFile.R")
+source("GSSFile.R")
+
+library(purrr)
+library(ContaminatedMixt)
+
+if (!requireNamespace("googledrive", quietly = TRUE)) {
+  install.packages("googledrive")
+}
+if (!requireNamespace("readr", quietly = TRUE)) {
+  install.packages("readr")
+}
+library(googledrive)
+library(gargle)
+library(readr)
+
+
+# Ubunto paths
+# pathScenarios <- "/home/jsancheg/Documents/Scenarios/"
+# pathFiles <- "/home/jsancheg/Documents/SSFiles/"
+
+# Windows path
+#pathScenarios <- "E:/University of Glasgow/Thesis/Scenarios/"
+#pathSSFiles <- "E:/University of Glasgow/Thesis/SSFiles/"
+#pathSFiles <- "E:/University of Glasgow/Thesis/SFiles/"
+
+
+dir(pathScenarios)
+ini <- n100.3+1 
+fin <- n100.4
+fin-ini + 1
+
+tic("SFiles n100.8+1 to n100.9")
+mclapply(Scenarios100[ini:fin], function(x){
+  
+  SFilename <- str_replace(x,"S_","SV_")
+  FilesProcessed <- dir(pathSFiles)
+#  if(is_empty(intersect(FilesProcessed,SFilename)))
+#  {
+    tryCatch(
+      {
+        GenerateSFile(x,pathScenarios,pathSFiles) 
+        return(1)
+      }, error = function(e){
+        cat("Error fitting scenario: ",x, "\n")
+        return(NULL)
+        
+      }
+    )
+    
+#  }else cat("\n The file ",SFilename, " already exists in the directory. \n")
+  
+}, mc.cores = 1)
+toc()
