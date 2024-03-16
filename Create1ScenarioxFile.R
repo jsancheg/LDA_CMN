eab48d031b7f45b65ed8f460fd16affb2d2cd728
jsancheg <jalfre81@hotmail.com>
2024-03-16 10:49:05 +0000

    Changes in Scenarios hard to fit

--- Create1ScenarioxFile.R
+++ Create1ScenarioxFile.R
@@ -1,9 +1,13 @@
 # The following code generate the scenarios in the cases that
 # one of the simulated scenarios of the pool of 10 stop the fitting at the others
 # scenarios
-
+source("utilities.R")
+source("Semisupervised.R")
 source("ListScenarios.R")
 source("SimulateScenario.R")
+source("GSFile.R")
+source("GSSFile.R")
+
 
 path_SSFilesOld <- "E:/University of Glasgow/Thesis/SSFiles_Old_PNolabeled50/"
 path_SFilesOld <- "E:/University of Glasgow/Thesis/SFiles_Old/"
@@ -45,11 +49,38 @@ sapply(1:n_scenarios_to_decompose,function(i)
 toc()
 
 
+scenarios_hard_to_fit <- dir(path_Scenarios_Hard_To_Fit)
 
 Model <- c("EII","VII","VEI","EEI","EVI","VVI","EEE","VVV")
 Model <- c("EII","VII","EEI","VEI","EEE","VVV")
 
-scenarios_hard_to_fit <- dir(path_Scenarios_Hard_To_Fit)
+# Generate Supervised files
+
+tic("SFiles_hard_to_fit")
+status <-mclapply(scenarios_hard_to_fit, function(x){
+  
+  SFilename <- str_replace(x,"S_","SSV_")
+  FilesProcessed <- dir(path_SFiles_Hard_To_Fit)
+  if(is_empty(intersect(FilesProcessed,SFilename))) 
+  {
+    tryCatch(
+      {
+        
+        GenerateSFile(x,path_Scenarios_Hard_To_Fit,path_SFiles_Hard_To_Fit,Model) 
+        return(1)
+      }, error = function(e){
+        cat("Error fitting scenario: ",x, "\n")
+        return(NULL)
+        
+      }
+    )
+    
+  }else cat("\n The file ",SFilename, " already exists in the directory. \n")
+  
+}, mc.cores = 1)
+toc()
+
+
 
 tic("SSFiles_hard_to_fit")
 status <-mclapply(scenarios_hard_to_fit, function(x){
@@ -76,9 +107,29 @@ status <-mclapply(scenarios_hard_to_fit, function(x){
 toc()
 
 
+# Update 
 
 
-# Simulate 20 simulations per Scenario
+files_no_generated_SSV_1 <- setdiff(str_replace(scenarios_hard_to_fit,"S_","SSV_"),dir(path_SSFiles_Hard_To_Fit) ) 
+files_no_generated_SSV
+
+files_no_generated_SV <- setdiff(str_replace(dir(pathScenarios),"S_","SV_"),dir(path_SFilesOld) ) 
+
+files_no_generated_SV
+
+ScenariosSSV_To_Be_Generated <-paste0("S",str_sub(files_no_generated_SSV,4,str_length(files_no_generated_SSV)))
+
+ScenariosSV_To_Be_Generated <-paste0("S",str_sub(files_no_generated_SV,3,str_length(files_no_generated_SV)))
+
+intersect(ScenariosSSV_To_Be_Generated,ScenariosSV_To_Be_Generated)
+
+setdiff(ScenariosSSV_To_Be_Generated,ScenariosSV_To_Be_Generated)
+
+
+Scenarios_to_be_generated <- unique(ScenariosSV_To_Be_Generated,ScenariosSSV_To_Be_Generated)
+
+
+# Simulate new 20 simulations per Scenario
 
 nSimulations <- 20
 
@@ -96,3 +147,26 @@ sapply( 2:length(Scenarios_to_be_generated), function (i) {
 })      
 
 
+tic("SSFiles_hard_to_fit")
+status <-mclapply(scenarios_hard_to_fit, function(x){
+  
+  SFilename <- str_replace(x,"S_","SSV_")
+  FilesProcessed <- dir(path_SFiles_Hard_To_Fit)
+  if(is_empty(intersect(FilesProcessed,SFilename))) 
+  {
+    tryCatch(
+      {
+        
+        GenerateSFile(x,path_Scenarios_Hard_To_Fit,path_SFiles_Hard_To_Fit,Model,pnolabeled = 0) 
+        return(1)
+      }, error = function(e){
+        cat("Error fitting scenario: ",x, "\n")
+        return(NULL)
+        
+      }
+    )
+    
+  }else cat("\n The file ",SFilename, " already exists in the directory. \n")
+  
+}, mc.cores = 1)
+toc()
